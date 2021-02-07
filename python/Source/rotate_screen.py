7c8c7f2df7c6580b25a48f56b4e9712944e26195
Nguyen Vincent <78664993+nguyen-v@users.noreply.github.com>
2021-02-07 01:01:08 +0100

    Update rotate_screen.py

--- python/Source/rotate_screen.py
+++ python/Source/rotate_screen.py
@@ -4,7 +4,6 @@
 #  Author:           Nguyen Vincent
 #  Created:          Feb, 03, 2021
 #  Updated:          Feb, 06, 2021
-#  Version:          1.0
 
 #  Hardware Resources:
 #  Sensor            LSM6DS3 Accelerometer and Gyroscope
